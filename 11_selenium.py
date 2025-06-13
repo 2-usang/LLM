1016570231f8c73b6a5423d8fe3ee5b6bb651122
이유상 <yusang157941@gmail.com>
2025-06-13 19:38:16 +0900

    야놀자 크롤링 마무리

--- 11_selenium.py
+++ 11_selenium.py
@@ -10,16 +10,29 @@ def crawl_yanolja_reviews(name, url):
 
     scroll_count = 3
     for i in range(scroll_count):
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')     # 스크롤을 끝까지 이동시켜
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')     # 자바스크립트 코드로 '스크롤을 끝까지 이동시켜'
         time.sleep(2)
 
     soup = BeautifulSoup(driver.page_source)
     review_containers = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div')
+    review_date = soup.select('#__next > section > div > div.css-1js0bc8 > div > div > div > div.css-1toaz2b > div > div.css-1ivchjf > p')
+    #__next > section > div > div.css-1js0bc8 > div > div > div > div.css-1toaz2b > div > div.css-1ivchjf > p
 
     for i in range(len(review_containers)):
-        print(i)
+        # print(i)
+        review_text = review_containers[i].find('p', class_='content-text').text
+        date = review_date[i].text
+        # print(review_text, date)
+        review_empty_stars = review_containers[i].find_all('path',{'fill-rule':'evenodd'})
+        stars = 5 -len(review_empty_stars)        
+        review_dict = {
+            'review':review_text,
+            'star':stars,
+            'date':date
+        }
+        review_list.append(review_dict)
 
-
+    print(review_list)
 
     time.sleep(5)
 
